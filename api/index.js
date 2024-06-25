73858edf2e6dbf2012ff6065bbd6182bbbe3c162
KittyCat11231 <kittycat61500@gmail.com>
2024-06-24 21:58:17 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.json({"foo": "bar"});
 })
 
+app.post('/echoback', (req, res) => {
+    res.json({"firstValue": req.body.firstValue});
+    res.json({"secondValue": req.body.secondValue});
+})
+
 app.listen(3000, () => {
     console.log(`Listening on port 3000.`);
 })
