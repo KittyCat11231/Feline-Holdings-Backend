449304c827a3bdb0bbea0ee8eae067774a86729b
KittyCat11231 <kittycat61500@gmail.com>
2024-06-25 12:56:47 -0400

    api for youtube

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const {google} = require('googleapis');
+
+const youtube = google.youtube({
+    version: 'v3',
+    auth: process.env.YT_API_KEY
+});
 
 app.get('/', (req, res) => {
     res.json({"foo": "bar"});
@@ -16,8 +25,21 @@ app.get('/intraroute', (req, res) => {
     res.send('IntraRoute API coming soon.');
 })
 
+app.get('/mbs/recent-videos', (req, res) => {
+    youtube.search.list({
+        part: 'snippet',
+        channelId: 'UCdqFWzZ2sTEM3svKajyk9Lg',
+        order: 'date',
+        maxResults: 10,
+    })
+    .then(results => {
+        res.send(results.data.items);
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
 app.listen(3000, () => {
     console.log(`Listening on port 3000.`);
 })
-
-module.exports = app;
