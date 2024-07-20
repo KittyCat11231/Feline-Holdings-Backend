095c0810b3b7658ba0051c6650f9ff3c66316e26
KittyCat11231 <kittycat61500@gmail.com>
2024-07-20 18:49:35 -0400

    starting server side IntraRoute

---
+++ routefinder/dijkstra.js
@@ -0,0 +1,92 @@
+const { helpers } = require('@kyle11231/helper-functions');
+
+function dijkstra(startId, endId, stopsData) {
+    let stopsMap = new Map();
+    let unexploredStops = [];
+
+    let allStops = helpers.deepCopy(stopsData);
+
+    allStops.forEach(stop => {
+        stopsMap.set(stop.id, stop);
+        stop.shortestTime = Infinity;
+        stop.path = [];
+        unexploredStops.push(stop);
+    })
+
+    class PathSegment {
+        constructor(stop1id, stop2id, routesIds, stopCount) {
+            this.stop1id = stop1id;
+            this.stop2id = stop2id;
+            this.routesIds = routesIds;
+            this.stopCount = stopCount;
+        }
+    }
+
+    let start = stopsMap.get(startId);
+    let end = stopsMap.get(endId);
+
+    start.shortestTime = 0
+    let currentStop = start;
+
+    while (unexploredStops.length > 0) {
+        let currentStopAtStartOfLoop = currentStop;
+
+        currentStop.adjacentStops.forEach(stop => {
+            // "adjStop" and "stop" refer to the same stop
+            // "adjStop" is for the stop from the main dataset
+            // "stop" is for the stop from currentStop.adjacentStops
+
+            let adjStop = stopsMap.get(stop.id);
+            let adjStopNewTime = currentStop.shortestTime + stop.weight;
+
+            let pathToAdjStop = new PathSegment(currentStop.id, stop.id, stop.routes, 1);
+            
+            if (adjStopNewTime < adjStop.shortestTime) {
+                adjStop.shortestTime = adjStopNewTime;
+                adjStop.path.push(pathToAdjStop);
+            }
+        });
+
+        unexploredStops = helpers.removeFromArray(currentStop, start);
+
+        let unexploredShortestTimes = [];
+
+        unexploredStops.forEach(stop => {
+            unexploredShortestTimes.push(stop.shortestTime);
+        });
+
+        let unexploredShortestTimeMin = Math.min(...unexploredShortestTimes);
+
+        unexploredStops.forEach(stop => {
+            if (stop.shortestTime === unexploredShortestTimeMin) {
+                currentStop = stop;
+                return;
+            }
+        })
+
+        if (currentStop === end) {
+            break;
+        }
+
+        if (currentStop.id === currentStopAtStartOfLoop.id) {
+            console.error('New current stop assignment failed.', 'currentStop:', currentStop, 'allStops', allStops);
+            break;
+        }
+    }
+
+    let path = helpers.deepCopy(end.path);
+
+    for (let i = 0; i < (path.length - 1);) {
+        if (helpers.deepEqual(path[i].routes, path[i + 1].routes)) {
+            path[i].stop2id = path[i + 1].stop2id;
+            path[i].stopCount += 1;
+            path = helpers.removeFromArray(path, path[i + 1]);
+        } else {
+            i++;
+        }
+    }
+
+    return path;
+}
+
+module.exports = { dijkstra };
