a706102d3c4a994e77841b9ff416f3804743c478
KittyCat11231 <kittycat61500@gmail.com>
2024-07-22 19:09:56 -0400

    Update k-shortest-paths.js

--- routefinder/k-shortest-paths.js
+++ routefinder/k-shortest-paths.js
@@ -1,79 +1,87 @@
 const { helpers } = require('@kyle11231/helper-functions');
 const { dijkstra } = require('./dijkstra');
-const { filterPathRoutes } = require('./filter-path-routes');
 
-function kShortestPaths(startId, endId, stopsData, numOfPaths) {
-    let shortestPath = dijkstra(startId, endId, stopsData);
-
-    // find second shortest for now
-
-    let possiblePaths = [];
-
-    class Path {
+function kShortestPaths(startId, endId, stopsData, kthShortest) {
+    class PathWithWeight {
         constructor(path, totalWeight) {
             this.path = path;
             this.totalWeight = totalWeight;
         }
     }
 
+    let shortestPath = dijkstra(startId, endId, stopsData);
+    let shortestPathTotalWeight = 0;
+
     shortestPath.forEach(segment => {
-        let allStops = helpers.deepCopy(stopsData);
-        let stopsMap = new Map();
+        shortestPathTotalWeight += segment.weight;
+    })
 
-        allStops.forEach(stop => {
-            stopsMap.set(stop.id, stop);
+    let shortestPathWithWeight = new PathWithWeight(shortestPath, shortestPathTotalWeight);
+
+    let paths = [shortestPathWithWeight];
+    let possiblePaths = [];
+
+    // if kthShortest is 1, it should return the shortest path.
+
+    let allStops = helpers.deepCopy(stopsData);
+    let stopsMap = new Map();
+
+    allStops.forEach(stop => {
+        stopsMap.set(stop.id, stop);
+    })
+
+    for (let k = 2; k <= kthShortest; k++) {
+        let currentShortestPath = paths[paths.length - 1];
+
+        currentShortestPath.forEach(currentSegment => {
+            // need to reset the value of allStops at the start of every loop pass
+            allStops = helpers.deepCopy(stopsData);
+            
+            spurNode = stopsMap.get(currentSegment.stop1id);
+            nodeAfterSpur = stopsMap.get(currentSegment.stop2id);
+
+            let pathToSpur = [];
+
+            currentShortestPath.forEach(segment => {
+                pathToSpur.push(segment);
+                if (segment.stop2id === spurNode) {
+                    return;
+                }
+            })
+
+            spurNode.adjacentStops.forEach(adjStop => {
+                if (adjStop.id === nodeAfterSpur.id) {
+                    adjStop.weight = Infinity;
+                    return;
+                }
+            })
+
+            let pathAfterSpur = dijkstra(spurNode.id, endId, allStops);
+            let combinedPath = pathToSpur.concat(pathAfterSpur);
+            let combinedPathTotalWeight = 0;
+            
+            combinedPath.forEach(segment => {
+                combinedPathTotalWeight += segment.weight;
+            })
+
+            possiblePaths.push(new PathWithWeight(combinedPath, combinedPathTotalWeight));
         })
 
-        let spurNode = stopsMap.get(segment.stop1id);
-        let nodeAfterSpur = stopsMap.get(segment.stop2id);
+        let candidatePath;
+        let candidatePathWeight = Infinity;
 
-        let pathToSpur = [];
-
-        shortestPath.forEach(pathToSpurSegment => {
-            pathToSpur.push(pathToSpurSegment);
-            if (pathToSpurSegment.stop2id === spurNode.id) {
-                return;
+        possiblePaths.forEach(path => {
+            if (path.weight < candidatePathWeight) {
+                candidatePath = path;
+                candidatePathWeight = path.weight;
             }
         })
-        
-        spurNode.adjacentStops.forEach(adjStop => {
-            if (adjStop.id === nodeAfterSpur.id) {
-                adjStop.weight = Infinity;
-                return;
-            }
-        });
 
-        let pathAfterSpur = dijkstra(spurNode.id, endId, allStops);
-        let combinedPath = pathToSpur.concat(pathAfterSpur);
-        let totalWeight = 0;
+        paths.push(candidatePath);
+        helpers.removeFromArray(possiblePaths, candidatePath);
+    }
 
-        combinedPath.forEach(segment => {
-            totalWeight += segment.weight;
-        })
-
-        let pathWithWeight = new Path(combinedPath, totalWeight);
-
-        possiblePaths.push(pathWithWeight);
-    })
-
-    let pathWeights = [];
-
-    possiblePaths.forEach(path => {
-        pathWeights.push(path.totalWeight);
-    })
-
-    let lowestWeight = Math.min(...pathWeights);
-    let secondShortestPath;
-
-    possiblePaths.forEach(path => {
-        if (path.totalWeight === lowestWeight) {
-            secondShortestPath = path;
-        }
-    })
-
-    secondShortestPath = filterPathRoutes(secondShortestPath);
-
-    return secondShortestPath;
+    return paths;
 }
 
 module.exports = { kShortestPaths };
