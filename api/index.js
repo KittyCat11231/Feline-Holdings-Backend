7ba0a44fcb3ee4723192f494aa241adc8bd0acce
KittyCat11231 <kittycat61500@gmail.com>
2024-07-25 00:17:01 -0400

    cors

--- api/index.js
+++ api/index.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://felineholdings.com/'
+}));
+
 const { MongoClient } = require('mongodb');
 const uri = require('../atlas_uri');
 
