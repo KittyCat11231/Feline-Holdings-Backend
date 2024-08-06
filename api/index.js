a2b85a8c9a0cd47e5ebec0c1da30cf9a9ae447ff
KittyCat11231 <kittycat61500@gmail.com>
2024-08-05 21:59:55 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'https://felineholdings.com'
+    origin: ['https://felineholdings.com', 'https://console.felineholdings.com']
 }));
 
 const { MongoClient } = require('mongodb');
