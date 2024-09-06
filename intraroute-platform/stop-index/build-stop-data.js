aa1e2c6b6dfcda3b5d279dbcb6f56830d5ccc94c
KittyCat11231 <63092830+KittyCat11231@users.noreply.github.com>
2024-09-05 20:46:17 -0400

    update intraroute-platform/stop-index/build-stop-data.js

--- intraroute-platform/stop-index/build-stop-data.js
+++ intraroute-platform/stop-index/build-stop-data.js
@@ -1,7 +1,29 @@
 const { helpers } = require('@kyle11231/helper-functions');
 
-function buildStopData(id, allStops, allRoutes) {
+function buildStopData(stopId, allStops) {
+    class Route {
+      constructor(id, meta1, meta2, stops) {
+        this.id = id;
+        this.meta1 = meta1;
+        this.meta2 = meta2;
+        this.stops = stops;
+      }
+    }
     
+    let stopsMap = new Map();
+    
+    for (let stop of allStops) {
+      stopsMap.set(stop.id, stop);
+    }
+    
+    let userStop = stopsMap.get(stopId);
+    
+    let routes = [];
+    
+    for (let route of userStop.routes) {
+      let stops = [];
+      routes.push(new Route(route.id, route.meta1, route.meta2, stops))
+    }
 }
 
 module.exports = { buildStopData };
